ac04c47bef586ba3cfb5322c2d1e3e7de7c063ec
Vikram413519 <timebond.logistics@gmail.comL>
2026-07-20 15:51:37 +0530

    Fix dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,6 @@ from sheet import get_scanner_data
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
 
@@ -29,11 +28,6 @@ def home():
         table=table
     )
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(
-        host="0.0.0.0",
-        port=port,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=port)
